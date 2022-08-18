7955a890d6edf06731a7a09e0f273f2b996bf5fe
Yuliya Zozulya <djulia@ukr.net>
2022-08-19 00:31:17 +0300

    Завершує виконання 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe);
+
+player.on('play', function() {
+console.log('played the video!');
+});
+
+const onPlay = function({seconds}) {
+   {
+    localStorage.setItem('videoplayer-current-time', seconds)   //  localStorage.getItem("videoplayer-current-time", seconds)
+    }
+};
+
+player.on('timeupdate', onPlay);
+
+const timeAfterUpdate = localStorage.getItem('videoplayer-current-time')
+
+player.setCurrentTime(timeAfterUpdate).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
