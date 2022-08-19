1eb63ad8862b5fcc27c24989ff63264f1ac8d17b
Yuliya Zozulya <djulia@ukr.net>
2022-08-19 23:16:03 +0300

    Виправляє слухача на формі у третьому завданні

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,32 +3,33 @@ import throttle from 'lodash.throttle';
 const feedbackForm = document.querySelector('.feedback-form');
 const userEmail = document.querySelector('.feedback-form input');
 const userFeedbackMessage = document.querySelector('.feedback-form textarea');
-const formSubmitBtn = document.querySelector('button');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-// feedbackForm.addEventListener('input', onTextareaInput)
-formSubmitBtn.addEventListener('submit', onFormSubmit)
+feedbackForm.addEventListener('input', throttle(onTextareaInput, 500))
+feedbackForm.addEventListener('submit', onFormSubmit)
+
+inputPushSavedTextareaMassage ()
 
 function onFormSubmit (evt) {
-   // evt.preventDefault();
-   console.log(evt)
-   // evt.currenttarget.reset();
-   // localStorage.removeItem(LOCAL_STORAGE_KEY);
+   evt.preventDefault();
+   console.log({ email: userEmail.value, message: userFeedbackMessage.value });
+   evt.target.reset();
+   localStorage.removeItem(LOCAL_STORAGE_KEY);
 }
 
-// inputPushSavedTextareaMassage ()
+function onTextareaInput (evt) {
+   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(
+      {email: userEmail.value, 
+      message: userFeedbackMessage.value}))
+}
 
-// function onTextareaInput (evt) {
-// localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({email: userEmail.value, message: userFeedbackMessage.value}))
-// }
-
-// function inputPushSavedTextareaMassage () {
-//    const unParsedLocalStorageKey = localStorage.getItem(LOCAL_STORAGE_KEY)
-//    const parsedLocalStorageKey = JSON.parse(unParsedLocalStorageKey)
+function inputPushSavedTextareaMassage () {
+   const unParsedLocalStorageValue = localStorage.getItem(LOCAL_STORAGE_KEY)
    
-// if (parsedLocalStorageKey.email !== '' || parsedLocalStorageKey.message !== '') {
-// userEmail.value = parsedLocalStorageKey.email
-// userFeedbackMessage.value = parsedLocalStorageKey.message
-// } 
-// }
-
+   if (unParsedLocalStorageValue) {
+   const parsedLocalStorageValue = JSON.parse(unParsedLocalStorageValue)
+   userEmail.value = parsedLocalStorageValue.email;
+   userFeedbackMessage.value = parsedLocalStorageValue.message;
+   }
+}
+  
