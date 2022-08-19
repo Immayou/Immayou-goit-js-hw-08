8d66dbe2a43be9e0d60e198a58772465e11ad50c
Yuliya Zozulya <djulia@ukr.net>
2022-08-19 15:03:17 +0300

    Виправляє порядок виведення функцій 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,34 @@
-const feedbackForm = document.querySelector('.feedback-form')
-const userEmail = document.querySelector('.feedback-form input')
-const userFeedbackMessage = document.querySelector('.feedback-form textarea')
-const formSubmitBtn = document.querySelector('button')
-const LOCAL_STORAGE_KEY = 'feedback-form-state'
+import throttle from 'lodash.throttle';
 
-feedbackForm.addEventListener('input', onTextareaInput)
+const feedbackForm = document.querySelector('.feedback-form');
+const userEmail = document.querySelector('.feedback-form input');
+const userFeedbackMessage = document.querySelector('.feedback-form textarea');
+const formSubmitBtn = document.querySelector('button');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+// feedbackForm.addEventListener('input', onTextareaInput)
 formSubmitBtn.addEventListener('submit', onFormSubmit)
 
-inputPushSavedTextareaMassage ()
-
-function onTextareaInput (evt) {
-localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({email: userEmail.value, message: userFeedbackMessage.value}))
-}
-
-function inputPushSavedTextareaMassage () {
-   const unParsedLocalStorageKey = localStorage.getItem(LOCAL_STORAGE_KEY)
-   const parsedLocalStorageKey = JSON.parse(unParsedLocalStorageKey)
-   
-if (unParsedLocalStorageKey) {
-userEmail.value = parsedLocalStorageKey.email
-userEmail
-} 
-}
-
 function onFormSubmit (evt) {
-evt.preventDefault();
-evt.currentTarget.reset();
-localStorage.removeItem();
+   // evt.preventDefault();
+   console.log(evt)
+   // evt.currenttarget.reset();
+   // localStorage.removeItem(LOCAL_STORAGE_KEY);
 }
+
+// inputPushSavedTextareaMassage ()
+
+// function onTextareaInput (evt) {
+// localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({email: userEmail.value, message: userFeedbackMessage.value}))
+// }
+
+// function inputPushSavedTextareaMassage () {
+//    const unParsedLocalStorageKey = localStorage.getItem(LOCAL_STORAGE_KEY)
+//    const parsedLocalStorageKey = JSON.parse(unParsedLocalStorageKey)
+   
+// if (parsedLocalStorageKey.email !== '' || parsedLocalStorageKey.message !== '') {
+// userEmail.value = parsedLocalStorageKey.email
+// userFeedbackMessage.value = parsedLocalStorageKey.message
+// } 
+// }
+
